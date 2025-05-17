5d67a8d0acbc5f4c917fe6ad095192ab24741fc6
gav3336 <gabriele.gavatta@gmail.com>
2025-05-17 20:06:10 +0200

    home page added

--- server/main.ts
+++ server/main.ts
@@ -1,8 +1,6 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { Hono } from 'npm:hono';
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const app = new Hono();
+
+
+Deno.serve({port: 8000}, app.fetch);
